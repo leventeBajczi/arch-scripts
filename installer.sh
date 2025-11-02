9e62c4e735ddfc5bb1118615cedbabd95ef2a0b9
Bajczi Levente <bajczilevi@gmail.com>
2025-11-02 13:22:34 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,52 +3,70 @@ set -e
 
 trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
-#ROOT=/dev/sda2
-#BOOT=/dev/sda1
-#HOME=/dev/sda3
-#SWAP=/dev/sda4
-#DEV=/dev/sda
+# Example device setup (adjust these before running)
+# ROOT=/dev/nvme0n1
+# BOOT=/dev/nvme0n1
+# SWAP=/dev/nvme0n1
+# DEV=/dev/nvme0n1
 
-ROOTMKFS="mkfs.xfs -f"
-BOOTMKFS="mkfs.vfat"
-HOMEMKFS="mkfs.xfs -f"
+ROOTMKFS="mkfs.btrfs -f -L archroot"
+BOOTMKFS="mkfs.vfat -F32"
 
+# --- Format and prepare root Btrfs filesystem ---
 $ROOTMKFS $ROOT
+
+# Mount temporarily to create subvolumes
 mount $ROOT /mnt
 
+# Create subvolumes
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@log
+btrfs subvolume create /mnt/@cache
+btrfs subvolume create /mnt/@snapshots
+
+# Unmount temporary mount
+umount /mnt
+
+# --- Mount subvolumes with recommended options ---
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@ $ROOT /mnt
+
+mkdir -p /mnt/{boot,home,var/log,var/cache/pacman/pkg,.snapshots}
+
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@home $ROOT /mnt/home
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@log $ROOT /mnt/var/log
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@cache $ROOT /mnt/var/cache/pacman/pkg
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@snapshots $ROOT /mnt/.snapshots
+
+# --- Mount EFI partition ---
 $BOOTMKFS $BOOT
-mkdir /mnt/boot -p
 mount $BOOT /mnt/boot
 
-if [ -z "$HOME" ]
-then
-    echo "No home"
-else
-    $HOMEMKFS $HOME
-    mkdir /mnt/home -p
-    mount $HOME /mnt/home
-fi
-
-if [ -z "$SWAP" ]
-then
+# --- Swap setup ---
+if [ -z "$SWAP" ]; then
     echo "No swap"
 else
     mkswap $SWAP
     swapon $SWAP
 fi
 
+# --- Mirrorlist & pacman setup ---
+HOME=$PWD reflector --country 'Germany,France' --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 
-HOME=$PWD reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
-printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
-
+# --- Install base system ---
 pacstrap /mnt - < pkglist.txt
 
+# --- Copy post-install files ---
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 mkdir -p /mnt/postinst
 cp aurpackages.txt /mnt/postinst/aurpackages.txt
 cp postinstall.sh /mnt/postinst/postinstall.sh
+
+# --- Generate fstab ---
 genfstab -U /mnt > /mnt/etc/fstab
 
+# --- Run postinstall inside chroot ---
 arch-chroot /mnt /postinst/postinstall.sh $DEV
 
-#reboot
+# Uncomment after testing
+# reboot
